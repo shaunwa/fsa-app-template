956b6c976048833637d96604ada80298fc334792
Shaun Wassell <shaun@trove.ai>
2021-08-10 20:48:22 -0400

    03.02 Created all pages and components, and added Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackButtonComponent } from './back-button/back-button.component';
 import { SmallXComponent } from './small-x/small-x.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { CreateGroupPageComponent } from './create-group-page/create-group-page.component';
+import { GroupsListPageComponent } from './groups-list-page/groups-list-page.component';
+import { GroupPageComponent } from './group-page/group-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GroupsListComponent } from './groups-list/groups-list.component';
+import { MyGroupsListComponent } from './my-groups-list/my-groups-list.component';
+import { MessagesListComponent } from './messages-list/messages-list.component';
+import { RequestsListComponent } from './requests-list/requests-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackButtonComponent,
-    SmallXComponent
+    SmallXComponent,
+    SignInPageComponent,
+    CreateGroupPageComponent,
+    GroupsListPageComponent,
+    GroupPageComponent,
+    NavBarComponent,
+    GroupsListComponent,
+    MyGroupsListComponent,
+    MessagesListComponent,
+    RequestsListComponent
   ],
   imports: [
     BrowserModule,
