c1c3ec1a073abd510f67e7b419b0f0a72e80ae94
Shaun Wassell <shaun@trove.ai>
2021-08-10 20:21:15 -0400

    Removed unneeded dropdown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackButtonComponent } from './back-button/back-button.component';
-import { DropDownComponent } from './drop-down/drop-down.component';
 import { SmallXComponent } from './small-x/small-x.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackButtonComponent,
-    DropDownComponent,
     SmallXComponent
   ],
   imports: [
